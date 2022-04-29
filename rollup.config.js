7cfebc580427d95f5d959cc6f2af83f1e6abcef5
Nick Bukovskyi <sonterixo@gmail.com>
2022-04-29 20:38:38 +0300

    Setup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,45 @@
+import external from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
-import styles from 'rollup-plugin-styles'
 import typescript from '@rollup/plugin-typescript'
+import styles from 'rollup-plugin-styles'
+import { terser } from 'rollup-plugin-terser'
 import dts from 'rollup-plugin-dts'
+
+const packageJson = require('./package.json')
 
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: 'dist/index.js',
-        name: 'index',
-        format: 'umd',
+        file: packageJson.main,
+        format: 'cjs',
         sourcemap: true
       }
     ],
     plugins: [
+      external(),
       resolve(),
       commonjs(),
       babel({
         babelHelpers: 'runtime',
-        exclude: '**/node_modules/**',
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        include: ['src/**/*']
       }),
       styles({
-        mode: 'extract',
         modules: true,
         minimize: true
       }),
       typescript({ tsconfig: './tsconfig.json' }),
-      dts()
+      terser()
     ]
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    external: [/\.scss$/],
+    plugins: [dts()]
   }
 ]
