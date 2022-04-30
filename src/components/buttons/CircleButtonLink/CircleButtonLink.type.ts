22fd98676cc77892d190bb5f1156a64bdf6358e5
Nick Bukovskyi <sonterixo@gmail.com>
2022-04-30 17:58:26 +0300

    Update Circle components with default props

--- src/components/buttons/CircleButtonLink/CircleButtonLink.type.ts
+++ src/components/buttons/CircleButtonLink/CircleButtonLink.type.ts
@@ -1,5 +1,7 @@
+import { Color, Layout, Size } from 'types/commonProps'
+
 export interface CircleButtonLinkProps extends React.ComponentProps<'a'> {
-  color?: 'black' | 'white' | 'yellow' | 'orange' | 'red' | 'rose' | 'green' | 'teal' | 'turquoise' | 'blue' | 'purple'
-  size?: 'sm' | 'md'
-  layout?: 'filled' | 'outlined'
+  color?: Color
+  size?: Size
+  layout?: Layout
 }
