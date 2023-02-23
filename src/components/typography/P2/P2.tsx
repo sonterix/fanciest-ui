5235e9c914401cd3884d69dc7e03a89383d3c50c
Nick Bukovskyi <sonterixo@gmail.com>
2023-02-23 14:07:47 -0700

    Fix disabled and P2 size

--- src/components/typography/P2/P2.tsx
+++ src/components/typography/P2/P2.tsx
@@ -5,7 +5,7 @@ import FontGenerator from '../FontGenerator/FontGenerator'
 import { getParams } from '../FontGenerator/helper'
 
 const P2 = ({ size, as, ...props }: FontProps): JSX.Element => {
-  return <FontGenerator size={getParams(size, 18)} as={as} selector="fui-p2" {...props} />
+  return <FontGenerator size={getParams(size, 16)} as={as} selector="fui-p2" {...props} />
 }
 
 export default P2
